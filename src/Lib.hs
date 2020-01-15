bac00d2e14a32d016efe63fe6216e4d746fabb4e
Mathieu Chauvin <mathieu.chauvin@orange.com>
2020-01-15 15:24:53 +0100

    Rover moves

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,36 @@
 module Lib
     ( Orientation(..)
-    , rover
+    , initialRover
+    , execute
     , demo
     ) where
 
 data Orientation = North | South | West | East deriving (Show, Eq)
 
-rover = ((0,0), North)
+initialRover = ((0,0), North)
+
+-- TODO remove replication
+execute 'F' ((x,y), North) = ((x,y+1), North)
+execute 'F' ((x,y), South) = ((x,y-1), South)
+execute 'F' ((x,y), West) = ((x-1,y), West)
+execute 'F' ((x,y), East) = ((x+1,y), East)
+execute 'B' ((x,y), North) = ((x,y-1), North)
+execute 'B' ((x,y), South) = ((x,y+1), South)
+execute 'B' ((x,y), West) = ((x+1,y), West)
+execute 'B' ((x,y), East) = ((x-1,y), East)
+
+execute 'L' (p, o) = (p, left o)
+execute 'R' (p, o) = (p, right o)
+
+left North = East
+left East = South
+left South = West
+left West = North
+
+right North = West
+right West = South
+right South = East
+right East = North
 
 demo :: IO ()
 demo = putStrLn "Not implemented yet..."
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,4 +5,24 @@ main :: IO ()
 main = hspec $ do
   describe "Rover" $ do
     it "shall initially by at origin, facing north" $ do
-      rover `shouldBe` ((0,0), North)
+      initialRover `shouldBe` ((0,0), North)
+    it "shall move forward" $ do
+      execute 'F' ((2,2), North)  `shouldBe` ((2,3), North)
+      execute 'F' ((2,2), South)  `shouldBe` ((2,1), South)
+      execute 'F' ((2,2), West)  `shouldBe` ((1,2), West)
+      execute 'F' ((2,2), East)  `shouldBe` ((3,2), East)
+    it "shall move backward" $ do
+      execute 'B' ((2,2), North)  `shouldBe` ((2,1), North)
+      execute 'B' ((2,2), South)  `shouldBe` ((2,3), South)
+      execute 'B' ((2,2), West)  `shouldBe` ((3,2), West)
+      execute 'B' ((2,2), East)  `shouldBe` ((1,2), East)
+    it "shall turn left" $ do
+      execute 'L' ((2,2), North)  `shouldBe` ((2,2), East)
+      execute 'L' ((2,2), East)  `shouldBe` ((2,2), South)
+      execute 'L' ((2,2), South)  `shouldBe` ((2,2), West)
+      execute 'L' ((2,2), West)  `shouldBe` ((2,2), North)
+    it "shall turn right" $ do
+      execute 'R' ((2,2), North)  `shouldBe` ((2,2), West)
+      execute 'R' ((2,2), West)  `shouldBe` ((2,2), South)
+      execute 'R' ((2,2), South)  `shouldBe` ((2,2), East)
+      execute 'R' ((2,2), East)  `shouldBe` ((2,2), North)
