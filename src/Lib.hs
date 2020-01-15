eb3f13657fb60cc3b5ed707f5fc27f50cf4765ab
Mathieu Chauvin <mathieu.chauvin@orange.com>
2020-01-15 11:45:05 +0100

    Rover initialisation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,12 @@
 module Lib
-    ( someFunc
+    ( Orientation(..)
+    , rover
+    , demo
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data Orientation = North | South | West | East deriving (Show, Eq)
+
+rover = ((0,0), North)
+
+demo :: IO ()
+demo = putStrLn "Not implemented yet..."
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,8 @@
+import Test.Hspec
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Rover" $ do
+    it "shall initially by at origin, facing north" $ do
+      rover `shouldBe` ((0,0), North)
